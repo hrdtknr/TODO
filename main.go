772fdc944da3d33724b47cf80c1a0e072489b9d3
rino_pk <rino@pk.com>
2020-09-02 13:33:19 +0900

    sql 01

--- main.go
+++ main.go
@@ -1,10 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"database/sql"
 
+	_ "github.com/go-sql-driver/mysql"
+)
 func main() {
 	fmt.Println("Hello, 世界")
 	fmt.Println("aaaaaa")
+
+//	db, err:=sql.Open("mysql", "root:1234@tcp(host:3306)/go")
+	db, err:=sql.Open("mysql", "root:@tcp(host:3306)/go")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("bbbbb")
+
+	defer db.Close() //defer:延期する
+
+	fmt.Println("ccccc")
+
+	rows, err:= db.Query("SELECT * FROM go.todo")
+
+	fmt.Println("ddddd")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("eeeee")
+
+	columns, err := rows.Columns()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("fff")
+
+	fmt.Println(columns)
+
+	fmt.Println("ggg")
 }
 
 //202009021150
