73dbdeb07be90181042d0914306eeacdd22574d5
rino_pk <rino@pk.com>
2020-09-03 13:28:57 +0900

    昼保存

--- main.go
+++ main.go
@@ -4,21 +4,92 @@ import (
 	"fmt"
 	"database/sql"
 //	"time"
+	"log"
+
+	"net/http"
+	"encoding/json"
+
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+var db, err =sql.Open("mysql", "root:1234@tcp(127.0.0.1:3306)/go")
+/*
+if err != nil {
+	panic(err.Error())
+}
+defer db.Close() //defer:延期する
+*/
+
 func main() {
 	fmt.Println("Hello")
 
-//	db, err:=sql.Open("mysql", "root:1234@tcp(host:3306)/localhost")
-//	db, err:=sql.Open("mysql", "root:@tcp(host:3306)/go")
-//	db, err:=sql.Open("mysql", "root:1234@tcp(host:3306)/go") tcp host -> 数値に
-	db, err:=sql.Open("mysql", "root:1234@tcp(127.0.0.1:3306)/go") //これでOK
-
-	if err != nil {
-		panic(err.Error())
+	/*
+	for i := 0; i < 2; i++ {
+		invokeMethod()
 	}
-	defer db.Close() //defer:延期する
+*/
+
+	http.HandleFunc("/test", TestHandler)
+	http.ListenAndServe(":8080", nil)
+
+
+	db.Close()//toriaezukokodeclose
+}
+
+type Todo struct {
+	ID int
+	Name string
+	Todo string
+}
+
+type Todos []Todo
+
+
+func TestHandler(w http.ResponseWriter, r *http.Request){
+/*
+	todo:= Todo {
+		ID: 1,
+		Name: "nameeeeeee",
+		Todo: "Todoooooooooo",
+	}
+*/
+
+	var todos Todos
+
+	rows, err:= db.Query("SELECT * FROM todo")
+	defer rows.Close()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for rows.Next(){
+		var id int
+		var  name string
+		var todo string
+		if err := rows.Scan(&id, &name, &todo); err != nil{
+			log.Println(err)
+			return
+		}
+//		fmt.Println(id, name, todo)
+
+		todoTmp:= Todo {
+			ID: id,
+			Name: name,
+			Todo: todo,
+		}
+
+		todos = append(todos, todoTmp)
+	}
+
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(todos)
+}
+
+func invokeMethod() {
 
 	fmt.Println("-----一覧表示-----")
 	read()
@@ -29,6 +100,7 @@ func main() {
 	fmt.Println("挿入: 1\n削除: 2\n更新: 3\n")
 	fmt.Scan(&input)
 
+	// web操作に切り替えるので、汚いけどとりあえず保留
 	switch(input) {
 		case 1:
 			fmt.Printf("入力: %d 処理: 挿入\n",input)
@@ -66,99 +138,44 @@ func main() {
 
 	fmt.Println("\n-----処理結果-----\n")
 	read()
-
-
-
-
-
-
-/*
-	//構造体を作ってデータを管理
-	type Todos struct{
-		ID int
-		Name string
-		Todo string
-	}
-	*/
 }
 
-//関数に分けるとdbの接続が切れる
-//後で修正するとして、各関数に接続処理を挿入
+
 
 
 func insert(name string, todo string){
-	//fmt.Println("insert")
-	//fmt.Println(name, todo)
-
-	//open
-	db, err:=sql.Open("mysql", "root:1234@tcp(127.0.0.1:3306)/go") //これでOK
-
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close() //defer:延期する
-
-	// insert
 	ins, err := db.Prepare("INSERT INTO todo(name, todo) VALUES(?,?)")
-	if err != nil {
-		panic(err.Error())
+	if err != nil { //error処理まででひとつのカタマリ
+		log.Println(err)
+		return
 	}
 	ins.Exec(name, todo)
 }
 
 func delete(id int){
-	//fmt.Println("delete")
-
-	//open
-	db, err:=sql.Open("mysql", "root:1234@tcp(127.0.0.1:3306)/go") //これでOK
-
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close() //defer:延期する
-
-	//delete
 	del, err := db.Prepare("DELETE FROM todo WHERE id = ?")
 	if err != nil {
-		panic(err.Error())
+		log.Println(err)
+		return
 	}
 	del.Exec(id)
 }
 
 func update(name string, todo string, id int){
-	// UPDATEの書き方はこれでOK(upAdteになってた)
-	//fmt.Println("UPDATE")
-
-	//open
-	db, err:=sql.Open("mysql", "root:1234@tcp(127.0.0.1:3306)/go") //これでOK
-
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close() //defer:延期する
-
-	//update
 	upd, err := db.Prepare("UPDATE todo SET name = ?, todo = ? WHERE id = ?")
 	if err != nil {
-		panic(err.Error())
+		log.Println(err)
+		return
 	}
 	upd.Exec(name, todo, id)
 }
 
 func read(){
-	//open
-	db, err:=sql.Open("mysql", "root:1234@tcp(127.0.0.1:3306)/go") //これでOK
-
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close() //defer:延期する
-
-	//read
 	rows, err:= db.Query("SELECT * FROM todo")
 	defer rows.Close()
 	if err != nil {
-		panic(err.Error())
+		log.Println(err)
+		return
 	}
 
 	for rows.Next(){
@@ -166,9 +183,9 @@ func read(){
 		var  name string
 		var todo string
 		if err := rows.Scan(&id, &name, &todo); err != nil{
-			panic(err.Error())
+			log.Println(err)
+			return
 		}
 		fmt.Println(id, name, todo)
 	}
 }
-
